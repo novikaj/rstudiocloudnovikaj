0a36ef9b4a618dd9cac27f9c3dc17294810f85b5
novikaj <novikaj@ukma.edu.ua>
2020-11-20 12:18:24 +0000

    multicollinearity rstudio cloud 20.11.2020

---
+++ multicollinearity.r
@@ -0,0 +1,15 @@
+install.packages("faraway")
+library(faraway)
+data(divusa)
+divusa
+?divusa
+head(divusa)
+dataworkshop<-data.frame(divusa[,-1])
+dataworkshop
+cor(dataworkshop)
+round(cor(dataworkshop),2)
+attach(dataworkshop)
+attach(divusa)
+divmodel<-lm(divorce~unemployed+femlab+marriage+birth+military)
+summary(divmodel)
+vif(divmodel)
